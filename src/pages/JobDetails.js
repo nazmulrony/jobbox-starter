c4d37e48be77c9b7d2a4421b201f8754be1683a8
nazmulrony <nhrony65@gmail.com>
2023-02-23 10:20:58 +0600

    reply to single question implemented

--- src/pages/JobDetails.js
+++ src/pages/JobDetails.js
@@ -67,9 +67,10 @@ const JobDetails = () => {
     };
 
     const [sendReply] = useReplyMutation();
-    const handleReply = (id) => {
+    const handleReply = (id, time) => {
         const data = {
             userId: id,
+            time,
             reply,
         };
         console.log(data);
@@ -148,38 +149,44 @@ const JobDetails = () => {
                             General Q&A
                         </h1>
                         <div className="text-primary my-2">
-                            {queries?.map(({ question, email, reply, id }) => (
-                                <div>
-                                    <small>{email}</small>
-                                    <p className="text-lg font-medium">
-                                        {question}
-                                    </p>
-                                    {reply?.map((item) => (
-                                        <p className="flex items-center gap-2 relative left-5">
-                                            <BsArrowReturnRight /> {item}
+                            {queries?.map(
+                                ({ question, email, reply, id, time }) => (
+                                    <div>
+                                        <small>{email}</small>
+                                        <p className="text-lg font-medium">
+                                            {question}
                                         </p>
-                                    ))}
-                                    {user.role === "employer" && (
-                                        <div className="flex gap-3 my-5">
-                                            <input
-                                                placeholder="Reply"
-                                                type="text"
-                                                className="w-full"
-                                                onBlur={(e) =>
-                                                    setReply(e.target.value)
-                                                }
-                                            />
-                                            <button
-                                                className="shrink-0 h-14 w-14 bg-primary/10 border border-primary hover:bg-primary rounded-full transition-all  grid place-items-center text-primary hover:text-white"
-                                                type="button"
-                                                onClick={() => handleReply(id)}
-                                            >
-                                                <BsArrowRightShort size={30} />
-                                            </button>
-                                        </div>
-                                    )}
-                                </div>
-                            ))}
+                                        {reply?.map((item) => (
+                                            <p className="flex items-center gap-2 relative left-5">
+                                                <BsArrowReturnRight /> {item}
+                                            </p>
+                                        ))}
+                                        {user.role === "employer" && (
+                                            <div className="flex gap-3 my-5">
+                                                <input
+                                                    placeholder="Reply"
+                                                    type="text"
+                                                    className="w-full"
+                                                    onBlur={(e) =>
+                                                        setReply(e.target.value)
+                                                    }
+                                                />
+                                                <button
+                                                    className="shrink-0 h-14 w-14 bg-primary/10 border border-primary hover:bg-primary rounded-full transition-all  grid place-items-center text-primary hover:text-white"
+                                                    type="button"
+                                                    onClick={() =>
+                                                        handleReply(id, time)
+                                                    }
+                                                >
+                                                    <BsArrowRightShort
+                                                        size={30}
+                                                    />
+                                                </button>
+                                            </div>
+                                        )}
+                                    </div>
+                                )
+                            )}
                         </div>
 
                         {user.role === "candidate" && (
